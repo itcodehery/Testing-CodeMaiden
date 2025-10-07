3065b59a82042a90f4486c2721b8c9166b8ef87e
Hari Prasad <haririo321@gmail.com>
2025-10-07 18:49:22 +0530

    ownership through functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-   let x = String::from("Hello World!"); 
-   println!("{}",x);
-   let y = x.clone();
-   println!("{} and {}",x,y);
+    let x = String::from("Hello World!");
+    println!("{}", x);
+    take_a_trip(&x);
+    println!("{}", x);
+}
+
+fn take_a_trip(x: &String) {
+    println!("\nTaking a trip with {}", x);
 }
